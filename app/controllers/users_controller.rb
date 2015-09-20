d65fc94a39ab25f4c55a1d9eedd52e99cf3fb794
Keita Shiratori <keita.shiratori@gmail.com>
2015-09-20 13:09:32 +0000

    create edit functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-
+  before_action :set_message, only: [:edit, :update, :destroy]
+  
   def show
    @user = User.find(params[:id])
   end
@@ -18,10 +19,31 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to current_user , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation,
+                                 :description, :location)
   end
+  
+  def set_message
+    @user=User.find(params[:id])
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  validates :description, length: { maximum: 500 }
+  validates :location, length: { maximum: 100 }
   has_secure_password
 end
